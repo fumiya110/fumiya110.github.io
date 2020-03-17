27c23f6ad2a68d9c42ffd2aac5bbc5c8e02e8cf5
fumiya110 <wecanchangeifwenevergiveup@gmail.com>
2020-03-17 02:44:15 +0000

    Add:topとbrief introductionの作成 その他各種調整(余白の調整やフォントの変更など)

--- portfolio.js
+++ portfolio.js
@@ -1,4 +1,14 @@
-$('#contact').on('click', function(){
+$('#intro-btn').on('click', function(){
+    let offset = $('#intro').offset().top;
+    $('html').animate({scrollTop:offset}, 1000);
+});
+
+
+$('#contact-btn').on('click', function(){
    let offset = $('footer').offset().top;
-   $('html').animate({scrollTop:offset}, 500);
+   $('html').animate({scrollTop:offset}, 1000);
+});
+
+$('.carousel').carousel({
+  interval: 5000
 });
