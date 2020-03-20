dd8d002179d4415caddfa0e011504a0e3c3ae4a6
fumiya110 <wecanchangeifwenevergiveup@gmail.com>
2020-03-20 10:50:06 +0000

    Add: Language Experienceの項目作成 ポートフォリオ以外に客観的に判断してもらえるように プログラミング経験について記載

--- portfolio.js
+++ portfolio.js
@@ -3,6 +3,15 @@ $('#intro-btn').on('click', function(){
     $('html').animate({scrollTop:offset}, 1000);
 });
 
+const lang_level= {'#html':'40%', '#python':'30%', '#ruby': '25%', '#css': '25%', '#sql': '10%', '#javascript':'10%'};
+$('#skill-btn').on('click', function(){
+    let offset = $('#skill').offset().top;
+    $('html').animate({scrollTop:offset}, 1000);
+    $.each(lang_level, function(property, value){
+       $(property).delay(900).animate({'width': value}, 1200, 'linear');
+       $(property).dequeue();
+    });
+});
 
 $('#contact-btn').on('click', function(){
    let offset = $('footer').offset().top;
@@ -12,3 +21,13 @@ $('#contact-btn').on('click', function(){
 $('.carousel').carousel({
   interval: 5000
 });
+
+
+
+
+
+
+
+
+
+
